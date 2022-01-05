baa644fbc03df4b236de632d5ffade555edee64f
EC2 Default User <ec2-user@ip-172-31-91-142.ec2.internal>
2022-01-05 14:35:28 +0000

    show.html.erbのエラー

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+
+  def index
+  end
+  
+  def new
+  end
+  
+  def create
+    @post=Post.new(params.require(:post).permit(:content,:user_id))
+    if @post.save
+      flash[:notice]="新規投稿しました"
+      redirect_to :users
+    else
+      render"posts"
+    end
+  end
+  
+  def show
+   @post = Post.find(params[:id])
+  end
+  
+  def edit
+   @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:content,:user_id))
+      flash[:notice] = "投稿を更新しました"
+      redirect_to :users
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+  end
+  
+end
+  
